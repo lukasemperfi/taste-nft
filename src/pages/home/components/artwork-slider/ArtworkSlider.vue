494d396c5e9f54e1292aabd78b72227df0b90f6f
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2026-03-21 21:36:38 +0200

    create slider

---
+++ src/pages/home/components/artwork-slider/ArtworkSlider.vue
@@ -0,0 +1,132 @@
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Pagination, EffectCreative } from 'swiper/modules'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/effect-creative'
+
+interface Artwork {
+  id: number
+  image: string
+  name: string
+  username: string
+  price: string
+}
+
+const props = defineProps<{ items: Artwork[] }>()
+const modules = [EffectCreative, Pagination]
+
+const updatePaginationWidth = (swiper: any) => {
+  const activeSlide = swiper.slides[swiper.activeIndex]
+
+  if (!activeSlide) {
+    return
+  }
+
+  const width = activeSlide.offsetWidth
+
+  const paginationEl = swiper.pagination?.el
+
+  if (!paginationEl) {
+    return
+  }
+
+  paginationEl.style.width = `${width}px`
+}
+</script>
+
+<template>
+  <swiper
+    :effect="'creative'"
+    :grabCursor="true"
+    :loop="true"
+    :slidesPerView="'auto'"
+    :loopAdditionalSlides="3"
+    :watchSlidesProgress="true"
+    :creativeEffect="{
+      limitProgress: 3,
+
+      prev: {
+        shadow: false,
+        translate: [0, 0, -200],
+        opacity: 0,
+        scale: 0.6,
+      },
+      next: {
+        translate: ['26.5%', 0, -150],
+        opacity: 1,
+        scale: 1,
+      },
+    }"
+    :pagination="{
+      el: '.custom-pagination',
+      clickable: true,
+    }"
+    @init="updatePaginationWidth"
+    @slideChange="updatePaginationWidth"
+    @resize="updatePaginationWidth"
+    :modules="modules"
+    class="artwork-swiper"
+  >
+    <swiper-slide v-for="item in items" :key="item.id" class="artwork-slide">
+      <div class="artwork-card">
+        <img :src="item.image" :alt="item.name" class="artwork-card__image" />
+      </div>
+    </swiper-slide>
+  </swiper>
+  <div class="custom-pagination"></div>
+</template>
+
+<style lang="scss" scoped>
+.artwork-swiper {
+  width: 100%;
+  height: 100%;
+}
+
+:deep(.swiper-slide) {
+  aspect-ratio: 1;
+  width: 38vw !important;
+  max-width: 519px;
+  max-height: 519px;
+}
+
+.artwork-card {
+  aspect-ratio: 1;
+  background: #c4c4c4;
+  border-radius: 4px;
+  box-shadow: 0px 10px 30px rgba(0, 0, 0, 0.5);
+  overflow: hidden;
+
+  &__image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    display: block;
+  }
+}
+
+.custom-pagination {
+  position: absolute;
+  bottom: -24px;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+  height: 5px;
+
+  :deep(.swiper-pagination-bullet) {
+    width: 60px;
+    height: 4px;
+    background: rgba(255, 255, 255, 0.2);
+    border-radius: 2px;
+    opacity: 1;
+    margin: 0 !important;
+    transition: all 0.3s ease;
+
+    &.swiper-pagination-bullet-active {
+      background: #fff;
+    }
+  }
+}
+</style>
---
+++ src/pages/home/sections/hero/Hero.vue
@@ -0,0 +1,119 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import ArtworkSlider from '../../components/artwork-slider/ArtworkSlider.vue'
+
+const artworks = ref([
+  {
+    id: 1,
+    name: 'WFH - art name first',
+    username: 'cryptopunk',
+    price: '0.34 ETH',
+    image: 'https://picsum.photos/id/101/600/600', // Замени на реальные пути
+  },
+  {
+    id: 2,
+    name: 'Abstract Geometry',
+    username: 'design_master',
+    price: '1.20 ETH',
+    image: 'https://picsum.photos/id/102/600/600',
+  },
+  {
+    id: 3,
+    name: 'Digital Ocean',
+    username: 'ocean_wave',
+    price: '0.55 ETH',
+    image: 'https://picsum.photos/id/103/600/600',
+  },
+  {
+    id: 4,
+    name: 'Neon Future',
+    username: 'cyber_artist',
+    price: '2.10 ETH',
+    image: 'https://picsum.photos/id/104/600/600',
+  },
+  {
+    id: 43,
+    name: 'Neon Future',
+    username: 'cyber_artist',
+    price: '2.10 ETH',
+    image: 'https://picsum.photos/id/105/600/600',
+  },
+  {
+    id: 42,
+    name: 'Neon Future',
+    username: 'cyber_artist',
+    price: '2.10 ETH',
+    image: 'https://picsum.photos/id/106/600/600',
+  },
+  {
+    id: 46,
+    name: 'Neon Future',
+    username: 'cyber_artist',
+    price: '2.10 ETH',
+    image: 'https://picsum.photos/id/99/600/600',
+  },
+  {
+    id: 4,
+    name: 'Neon Future',
+    username: 'cyber_artist',
+    price: '2.10 ETH',
+    image: 'https://picsum.photos/id/104/600/600',
+  },
+  {
+    id: 43,
+    name: 'Neon Future',
+    username: 'cyber_artist',
+    price: '2.10 ETH',
+    image: 'https://picsum.photos/id/105/600/600',
+  },
+  {
+    id: 42,
+    name: 'Neon Future',
+    username: 'cyber_artist',
+    price: '2.10 ETH',
+    image: 'https://picsum.photos/id/106/600/600',
+  },
+  {
+    id: 46,
+    name: 'Neon Future last',
+    username: 'cyber_artist',
+    price: '2.10 ETH',
+    image: 'https://picsum.photos/id/99/600/600',
+  },
+])
+</script>
+<template>
+  <section class="hero">
+    <div class="hero__container app-container">
+      <div class="hero__col-1"></div>
+      <div class="hero__col-2">
+        <ArtworkSlider :items="artworks" />
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped lang="scss">
+.hero {
+  &__container {
+    display: flex;
+    gap: 16px;
+    padding-block: 24px;
+  }
+
+  &__col-1 {
+    flex: 1 1 516px;
+
+    max-width: 516px;
+  }
+
+  &__col-2 {
+    flex: 1 1 786px;
+
+    max-width: 786px;
+    max-height: 519px;
+    aspect-ratio: 786 / 519;
+    min-width: 0;
+    position: relative;
+  }
+}
+</style>
