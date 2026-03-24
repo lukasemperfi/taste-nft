e5f4fa125932c1e30cacc3363e63d4fa7a1565ce
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2026-03-24 23:16:24 +0200

    create creator works list

--- src/pages/creator-profile/CreatorProfile.vue
+++ src/pages/creator-profile/CreatorProfile.vue
@@ -2,6 +2,7 @@
 import { useRoute } from 'vue-router'
 import { computed } from 'vue'
 import CreatorInfo from './sections/CreatorInfo.vue'
+import WorkTabs from './sections/WorkTabs.vue'
 
 const route = useRoute()
 const creatorId = computed(() => route.params.id || '1')
@@ -12,12 +13,18 @@ console.log('creatorId', creatorId.value)
 <template>
   <div class="creator-profile">
     <CreatorInfo />
+    <WorkTabs />
   </div>
 </template>
 <style scoped lang="scss">
 .creator-profile {
   :deep(.creator-info) {
     margin-top: 39px;
+  }
+
+  :deep(.work-tabs) {
+    margin-top: 26px;
+    margin-bottom: 51px;
   }
 }
 </style>
---
+++ src/pages/creator-profile/sections/WorkTabs.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Tabs from '@/molecules/tabs/Tabs.vue'
+import TabsTrigger from '@/molecules/tabs/TabsTrigger.vue'
+import TabsPanel from '@/molecules/tabs/TabsPanel.vue'
+import TabsList from '@/molecules/tabs/TabsList.vue'
+import WorksList from '../components/WorksList.vue'
+import { artworks, artworksRandom } from '@/helpers/artworks'
+
+const activeTab = ref('created')
+</script>
+<template>
+  <div class="work-tabs app-container">
+    <Tabs v-model="activeTab">
+      <TabsList>
+        <TabsTrigger id="created">Created </TabsTrigger>
+        <TabsTrigger id="collected">Collected </TabsTrigger>
+      </TabsList>
+
+      <div class="tabs-content-container">
+        <TabsPanel id="created">
+          <WorksList :list="artworks" />
+        </TabsPanel>
+
+        <TabsPanel id="collected">
+          <WorksList :list="artworksRandom" />
+        </TabsPanel>
+      </div>
+    </Tabs>
+  </div>
+</template>
+<style scoped lang="scss">
+.work-tabs {
+  :deep(.tabs-list) {
+    margin-bottom: 23px;
+    .tab-trigger {
+      font-family: var(--font-family);
+      font-weight: 700;
+      font-size: clamp(16px, 1.3vw, 18px);
+    }
+  }
+
+  :deep(.user-identity__name) {
+    color: #1d2228;
+  }
+}
+</style>
